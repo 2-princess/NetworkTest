af1589c52433ac75ff606e005ddb3b7a7c2a4c90
2Princess <ww@jeongdawon-ui-MacBookPro.local>
2026-08-05 04:58:16 +0900

    NetWorkVariable

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -3,19 +3,42 @@ using UnityEngine;
 
 public class PlayerMove : NetworkBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    public Renderer playerRenderer;
+    private NetworkVariable<int> colorIndex = new NetworkVariable<int>();
 
+    public override void OnNetworkSpawn()
+    {
+        colorIndex.OnValueChanged += ChangeColor;
+
+        if (IsServer)
+        {
+            colorIndex.Value = (int)OwnerClientId;
+        }
+
+        ChangeColor(0, colorIndex.Value);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (!IsOwner) return;
+        if (IsServer)
+        {
+            colorIndex.Value = (int)OwnerClientId;
+        }
 
         float h = Input.GetAxisRaw("Horizontal");
-
         transform.position += Vector3.right * h * 3f * Time.deltaTime;
+    }
+
+    private void ChangeColor(int oldValue, int newValue)
+    {
+        if (newValue == 0)
+        {
+            playerRenderer.material.color = Color.blue;
+        }
+        else
+        {
+            playerRenderer.material.color = Color.red;
+        }
     }
 }

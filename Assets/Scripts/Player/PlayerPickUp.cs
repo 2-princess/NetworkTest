62c6023ef18fdc82d6a7847f3000d4a828315f6c
2Princess <ww@jeongdawon-ui-MacBookPro.local>
2026-08-08 10:27:28 +0900

    PockUp

---
+++ Assets/Scripts/Player/PlayerPickUp.cs
@@ -0,0 +1,32 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerPickUp : NetworkBehaviour
+{
+    public Transform handPos;
+    // Update is called once per frame
+    void Update()
+    {
+        if (!IsOwner) return;
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            PickUpRPC();
+        }
+    }
+
+    [Rpc(SendTo.Server)]
+    private void PickUpRPC()
+    {
+        Collider[] hits = Physics.OverlapSphere(handPos.position, 2f);
+        foreach (Collider item in hits)
+        {
+            if (item.CompareTag("Item"))
+            {
+                NetworkObject netObj = item.GetComponent<NetworkObject>();
+                if (netObj == null) continue;
+                netObj.Despawn();
+                break;
+            }
+        }
+    }
+}
